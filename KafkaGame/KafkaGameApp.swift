7e05a583c5df4de8830c06ba430661a7fb1a8b99
Toby Loki <tobyloki5@gmail.com>
2023-02-20 13:18:54 -0800

    Initial Commit

---
+++ KafkaGame/KafkaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  KafkaGameApp.swift
+//  KafkaGame
+//
+//  Created by Alex on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct KafkaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
